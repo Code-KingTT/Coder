1e92b39e759cae84180c58dfe5a6a982f88c3387
王通通 <2973912405@qq.com>
2025-08-14 00:37:16 +0800

    用户模块引入公共模块

--- coder-server/coder-user/src/main/java/com/coder/UserApplication.java
+++ coder-server/coder-user/src/main/java/com/coder/UserApplication.java
@@ -4,7 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+/**
+ * 用户服务启动类
+ *
+ * @author Sunset
+ * @date 2025/8/13
+ */
+@SpringBootApplication(scanBasePackages = "com.coder")
 @EnableDiscoveryClient
 public class UserApplication {
     public static void main(String[] args) {
