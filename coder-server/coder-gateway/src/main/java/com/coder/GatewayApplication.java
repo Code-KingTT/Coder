2f4a736987bcb2f89582b6112434dbe459b511a6
king-zishuo <wang_ttong@inforbus.com>
2025-08-20 14:32:22 +0800

    修复网关模块引入公共模块的依赖冲突问题

--- coder-server/coder-gateway/src/main/java/com/coder/GatewayApplication.java
+++ coder-server/coder-gateway/src/main/java/com/coder/GatewayApplication.java
@@ -1,11 +1,18 @@
 package com.coder;
 
+import com.coder.exception.GlobalExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+                classes = GlobalExceptionHandler.class)
+})
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
