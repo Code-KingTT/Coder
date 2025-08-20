6874faa04e0f5e35b86ce911bbba0a9f19840c4f
king-zishuo <wang_ttong@inforbus.com>
2025-08-20 14:23:42 +0800

    删除认证启动类的Knif4j打印信息

--- coder-server/coder-auth/src/main/java/com/coder/AuthApplication.java
+++ coder-server/coder-auth/src/main/java/com/coder/AuthApplication.java
@@ -19,9 +19,5 @@ public class AuthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
-        System.out.println("=================================");
-        System.out.println("认证服务启动成功！");
-        System.out.println("Swagger文档地址: http://localhost:8082/doc.html");
-        System.out.println("=================================");
     }
 }
